8cffc258ca7c8c875ec11638dfcce30f7234c098
rockkoca@gmail.com <end0405LESS>
2017-03-28 16:53:31 -0700

    click to add wizard press right arrow to add basic walker press up arrow to add quick walker
    wizard could not fire yet!

--- Sample.java
+++ Sample.java
@@ -3,22 +3,46 @@ import HTLProceduralAPI.*;
 
 public class Sample extends HTLProceduralAPI {
 
-	public void buildGame() {
-				
-		for (int i = 0; i < 20; i++) {
-			addPathLeftRight(i, 5);
-		}
+    public void buildGame() {
+        drawToolbars();
 
-	}
+        for (int i = 0; i < 7; i++) {
+            addPathLeftRight(i, 5);
+        }
 
-	public void updateGame() {
+        // can wrap the corner into a function later
+        addPathDownLeft(7, 5);
+        addPathUpDown(7, 4);
+        addPathUpRight(7, 3);
+        addPathLeftRight(8, 3);
+        addPathUpLeft(9, 3);
+        addPathUpDown(9, 4);
+        addPathDownRight(9, 5);
 
+        for (int i = 10; i < 20; i++) {
+            addPathLeftRight(i, 5);
+        }
 
-		if (mouseClicked()) {
-		
-			drawWizard(getClickedX(), getClickedY(), "medic");
-		
-		}
+        // set the start and the path for walkers
+        preparePathForWalkers(0, 5, 19, 5);
+    }
 
-}
+    public void updateGame() {
+
+        // click screen to add medic wizard
+        if (mouseClicked()) {
+            int x = getClickedX();
+            int y = getClickedY();
+            drawWizard(x, y, "medic");
+        }
+
+        // press right arrow to add walkers
+        if (keyboardIsPressingRight()) {
+            addWalker("basic");
+        } else if (keyboardIsPressingUp()){
+            // press up arrow to add quick walkers
+            addWalker("quick");
+        }
+
+    }
 }
